6270021d6c6678c7a47ac52485a2074c1b81ded8
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2022-11-25 15:51:49 +0000

    With get or create

--- CachingSample/Controllers/HomeController.cs
+++ CachingSample/Controllers/HomeController.cs
@@ -1,21 +1,29 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using CachingSample.Models;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace CachingSample.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IMemoryCache _cache;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger,
+    IMemoryCache memoryCache)
     {
         _logger = logger;
+        _cache = memoryCache;
     }
 
     //[ResponseCache(Duration=10)]
     public IActionResult Index()
     {
+        ViewData["TheTime"] = _cache.GetOrCreate("TheTime", (entry) => {
+            entry.SlidingExpiration = TimeSpan.FromSeconds(7);
+            return DateTime.Now;
+        });
         return View();
     }
 
