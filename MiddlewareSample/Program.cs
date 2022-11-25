8444787b8431a861e0a6179ef6cb30fc805142dc
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2022-11-25 10:59:12 +0000

    Multiple middlewares

--- MiddlewareSample/Program.cs
+++ MiddlewareSample/Program.cs
@@ -3,11 +3,14 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.Use(async (context, next) => {
-    //await context.Response.WriteAsync("===");
+    await context.Response.WriteAsync("===");
     await next();
     await context.Response.WriteAsync("^^^");
 });
 
-app.MapGet("/", () => "Hello .NETworking!");
+app.Use(async (HttpContext context, Func<Task> next) => {
+await context.Response.WriteAsync("Hey .NETWorking");
+});
+
 
 app.Run();
