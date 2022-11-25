c4613a1bc5d505514693e0107f6b82e9a3acf346
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2022-11-25 10:39:14 +0000

    Adding middleware project

---
+++ MiddlewareSample/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MiddlewareSample.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
