0489a9316c52dc432ad0cd395024914c5a7e4a8e
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2022-11-25 10:34:18 +0000

    Adding background worker

--- HostedServiceSample/Program.cs
+++ HostedServiceSample/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSingleton<IHostedService, SampleHostedService>();
+builder.Services.AddHostedService<SampleBackgroundService>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
